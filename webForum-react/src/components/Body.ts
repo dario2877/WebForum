6e2529d2435fab414094cf8e53b24c81d7f62c96
Dario O <38870925+dario2877@users.noreply.github.com>
2026-01-12 14:06:05 -0500

    adding the body  component

---
+++ webForum-react/src/components/Body.ts
@@ -0,0 +1,15 @@
+import { react } from "react";
+import { comments } from "../assets/commentData";
+
+
+
+type BodyProps = {
+    listOfObjects: typeof comments
+  
+};
+export function Body(props: BodyProps) {
+  return (
+
+
+);
+}
