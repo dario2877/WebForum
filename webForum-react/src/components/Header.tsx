ac5a61232f66dc90a004423167bde3282e2beb98
Dario O <38870925+dario2877@users.noreply.github.com>
2026-01-21 17:59:10 -0500

    Adding the header component

---
+++ webForum-react/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type HeaderProps = {
+  profilePicUrl: string;
+  username: string;
+};
+
+
+export function Header({ profilePicUrl, username }: HeaderProps) {
+  return (
+    <>
+    <img src={profilePicUrl} alt={username} />
+    <h1>{username}</h1>
+    </>
+  );
+}
