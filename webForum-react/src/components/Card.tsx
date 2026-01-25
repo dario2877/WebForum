e47ddd12c8dda3eee382b97c499fdb04bc6d7388
Dario O <38870925+dario2877@users.noreply.github.com>
2026-01-25 12:53:53 -0500

    Card App,Header component

--- webForum-react/src/components/Card.tsx
+++ webForum-react/src/components/Card.tsx
@@ -1,3 +1,18 @@
 import { Body } from "./Body";
+import { Header } from "./Header";
 
-
+export function Card({
+  commentObject,
+}: {
+  commentObject: { profileImg: string; username: string; comment: string };
+}) {
+  return (
+    <div>
+      <Header
+        profilePicUrl={commentObject.profileImg}
+        username={commentObject.username}
+      />
+      <Body listOfComments={[commentObject]} />
+    </div>
+  );
+}
